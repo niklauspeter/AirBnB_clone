c580a7d7554a2995e6c55e71093db809ad083b37
klaus <oriokiklaus@gmail.com>
2023-03-14 16:23:40 +0300

    make pycodestyle compliant

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,20 @@
+"""defines the BaseModel Class"""
+import models
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
     """Represents the BaseModel of the HBnB project."""
-
-
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-         """Return the print/str representation of the BaseModel instance."""
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        """Return the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
 
     def save(self):
         """Update updated_at with the current datetime."""
